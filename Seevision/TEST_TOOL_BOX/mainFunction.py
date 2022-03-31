15aa145657fb2bea32595fdbed42957c59637be7
CHENGUANGTAO <792607724@qq.com>
2022-03-31 15:29:29 +0800

    Add explanation for bad point check pyqt5 tool

---
+++ Seevision/TEST_TOOL_BOX/mainFunction.py
@@ -0,0 +1,61 @@
+# coding = utf8
+import os
+import subprocess
+import sys
+
+from PyQt5 import QtWidgets
+
+from Seevision.TEST_TOOL_BOX.SeevisionToolBox import Ui_MainWindow
+
+os.path.abspath(".")
+
+# 设置递归限制，防止程序中的循环使得python内存溢出
+sys.setrecursionlimit(5000)
+"""
+    @Project:PyQt5Project
+    @File:mainFunction.py
+    @Author:十二点前要睡觉
+    @Date:2022/3/31 15:05
+"""
+
+
+def HidToolToolBar(btn_type):
+    software_path = ""
+    if btn_type == "hidTool2_4":
+        software_path = r"./Tools/HIDTools_2_4/HIDTool.exe"
+    elif btn_type == "hidTool2_5":
+        software_path = r"./Tools/HIDTools_2_5/HIDTool_2_5.exe"
+    launchSoftware(software_path)
+
+
+def ScriptListControlBar():
+    pass
+
+
+def ScriptControlBar():
+    pass
+
+
+def launchSoftware(software_path):
+    global software
+    software = subprocess.Popen(software_path)
+
+
+def closeSoftware():
+    if software:
+        software.kill()
+
+
+def ui_connect():
+    ui.btn_hidTool_2_4.clicked.connect(lambda: HidToolToolBar("hidTool2_4"))
+    ui.btn_hidTool_2_5.clicked.connect(lambda: HidToolToolBar("hidTool2_5"))
+
+
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    mainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(mainWindow)
+    ui_connect()
+    mainWindow.show()
+    sys.exit(app.exec_())
